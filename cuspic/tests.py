ec2b2dc3aae8863e37e54fe560c40e49c4544f9a
Ubuntu <ubuntu@ip-172-31-80-230.ec2.internal>
2019-07-05 08:04:24 +0000

    tests added for cuspic model

--- cuspic/tests.py
+++ cuspic/tests.py
@@ -7,12 +7,15 @@ class CusPicTests(TestCase):
     tests to run against CusPic models
     
     """
-    def test_str(self):
-        test_event = CusPic(event_name='LeMan')
-        self.assertEqual(str(test_event),'LeMan')
+
     
-    def test_str_id(self):
-        test_id = CusPic(uploader_id='3')
-        self.assertEqual(str(test_id),'3')
+    def test_adding_a_pic(self):
+        test_pic = CusPic(event_name='LeMan', uploader_id='3', price=2.99, info='here is some info',image='test.jpg')
+        test_pic.save
+        self.assertEqual(test_pic.event_name,'LeMan')
+        self.assertEqual(test_pic.uploader_id,'3')
+        self.assertEqual(test_pic.price,2.99)
+        self.assertEqual(test_pic.info,'here is some info')
+        self.assertEqual(test_pic.image,'test.jpg')
     
   
