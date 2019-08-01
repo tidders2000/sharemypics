c0a99fc1f45fa5ab7fdf40f54b41c465f4c8e8e5
tidders2000 <simon.tid@btinternet.com>
2019-08-01 12:48:57 +0000

    styled cart link on nav bar

--- cuspic/models.py
+++ cuspic/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CusPic (models.Model):
     event_name = models.CharField(max_length=254, default='')
     username = models.CharField(max_length=254, blank=True)
-    uploader_id = models.CharField(max_length=8)
+    uploader_id = models.IntegerField()
     image = models.ImageField(upload_to='media/images')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     info = models.TextField()
