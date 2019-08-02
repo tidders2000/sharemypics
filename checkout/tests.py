c0c0a1f0ce98b7986e9ceb000e5709b8a5840a60
tidders2000 <simon.tid@btinternet.com>
2019-08-02 21:53:00 +0000

    tests added to checkout

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+csrf_client = Client(enforce_csrf_checks=True)
+class TestViews(TestCase):
 
-# Create your tests here.
+ def test_get_checkout_not_logged_in(self):
+    
+    c = Client()
+    page = self.client.get("/checkout",follow=True)
+    self.assertEqual(page.status_code,200 )
+    self.assertTemplateUsed(page, "login.html")
+    
