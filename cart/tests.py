402d8c3eeda069b701a5a92f64f46dffc2dba0c9
tidders2000 <simon.tid@btinternet.com>
2019-08-02 21:40:27 +0000

    tests for cart added

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
+csrf_client = Client(enforce_csrf_checks=True)
+
+class TestViews(TestCase):
+        """
+        tests to run against cart
+        
+        """
 # Create your tests here.
+def test_cart_view(self):
+        page = self.client.get("/cart/", follow=True)
+        self.assertEqual(page.status_code, 200)
+        
+def test_add_to_cart(self):
+    
+         c = Client()
+         page = c.get('add_to_cart', {'id': 1})
+         self.assertEqual(page.status_code, 200)
+
+def test_add_to_cart_nothing(self):
+         c = Client()
+         page = c.get('add_to_cart', {})
+         self.assertEqual(page.status_code, 200)
