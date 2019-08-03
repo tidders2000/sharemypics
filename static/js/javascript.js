8da8db6c15c0c31676b2f6740b0f98b324c83453
tidders2000 <simon.tid@btinternet.com>
2019-08-03 11:54:13 +0000

    comments added to js

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,10 +1,14 @@
 	jQuery(document).ready(function($) {
+		
+	//lightcase initiator
+	
 		$('a[data-rel^=lightcase]').lightcase({
 		    
 		    closeOnOverlayClick: false,
 		   
 		   
 		});
+	// disable right click
 		$(document).bind("contextmenu",function(e){
     return false;
 });
