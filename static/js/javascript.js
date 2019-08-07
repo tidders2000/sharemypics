50fc88772a73e86d638073264e48a9bd73762a07
tidders2000 <simon.tid@btinternet.com>
2019-08-07 21:49:24 +0000

    added jquery validate

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -14,7 +14,36 @@
 });
 	
 	
+$(function() {
 	
+  //initialise form to stop 0 quantity being entered by user
+  
+  $("#quantity_form").validate({
+    //  validation rule
+    rules: {
+      
+      quantity:{
+      	required: true,
+        min: 1
+      },
+     
+     
+    
+    },
+    // error message
+    messages: {
+      quantity: "Must be greater than 0",
+      
+    },
+    errorElement: 'div',
+    errorLabelContainer: '.errortxt',
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});	
 
 	});
 
