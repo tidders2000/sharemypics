73d191b221af1053ccfbef535d0d5bbb06a7615a
tidders2000 <simon.tid@btinternet.com>
2019-08-28 11:04:06 +0000

    fixed bug on index event search

--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, reverse
 from cuspic.models import CusPic
+from django.contrib import messages
+from django.contrib import auth
+import os
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 """ passes list of events in the DB to the index page """
 def index(request):
     
     events = CusPic.objects.values('event_name').distinct()
-    user=request.user.username
-    return render(request, "index.html", {'user':user},{'events':events})
+    
+    return render(request, "index.html", {'events':events})
