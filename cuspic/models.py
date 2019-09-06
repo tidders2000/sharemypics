bbfc3b7f2f8b722b2aec9949b22189fe18b434db
tidders2000 <simon.tid@btinternet.com>
2019-09-06 19:45:22 +0000

    fixed min credit card value bug

--- cuspic/models.py
+++ cuspic/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import  MinValueValidator
 # Create your models here.
 class CusPic (models.Model):
     event_name = models.CharField(max_length=254, default='')
     user = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='media/images')
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0.35)])
     info = models.TextField()
     wm_image = models.ImageField(upload_to='media/images/watermarks',blank=True)
     
