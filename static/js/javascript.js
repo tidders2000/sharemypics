3a0f709bac6146fc7e1fddd1559d92bc17dfdeaf
tidders2000 <simon.tid@btinternet.com>
2019-09-07 17:23:44 +0000

    styled alert box and added close button

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,69 +1,71 @@
 	jQuery(document).ready(function($) {
-	  
+
 	  $('.toast').toast()
 	  $('document').toast('show')
-	  $('.has-popover').popover({'trigger':'hover'});
 
-		
-$("a#single_image").fancybox();
-	
-	/* Using custom settings */
-	
-	$("a#inline").fancybox({
-		'hideOnContentClick': true
+	  /*hide alert box */
+	  $('#fade').click(function() {
+	    $('#alert').hide();;
+	  });
+
+
+	  /*fancy box standard code */
+
+	  $("a#single_image").fancybox();
+
+	  /* Using custom settings */
+
+	  $("a#inline").fancybox({
+	    'hideOnContentClick': true
+	  });
+
+	  /* Apply fancybox to multiple items */
+
+	  $("a.group").fancybox({
+	    'transitionIn': 'elastic',
+	    'transitionOut': 'elastic',
+	    'speedIn': 600,
+	    'speedOut': 200,
+	    'overlayShow': false
+	  });
+
+
+	  // disable right click
+	  $(document).bind("contextmenu", function(e) {
+	    return false;
+	  });
+
+
+	  $(function() {
+
+	    //initialise form to stop 0 quantity being entered by user
+
+	    $("#quantity_form").validate({
+	      //  validation rule
+	      rules: {
+
+	        quantity: {
+	          required: true,
+	          min: 1
+	        },
+
+
+
+	      },
+	      // error message
+	      messages: {
+	        quantity: "Must be greater than 0",
+
+	      },
+	      errorElement: 'div',
+	      errorLabelContainer: '.errortxt',
+	      // Make sure the form is submitted to the destination defined
+	      // in the "action" attribute of the form when valid
+	      submitHandler: function(form) {
+	        form.submit();
+	      }
+	    });
+	  });
+
 	});
-
-	/* Apply fancybox to multiple items */
 	
-	$("a.group").fancybox({
-		'transitionIn'	:	'elastic',
-		'transitionOut'	:	'elastic',
-		'speedIn'		:	600, 
-		'speedOut'		:	200, 
-		'overlayShow'	:	false
-	});
-	
-
-	// disable right click
-		$(document).bind("contextmenu",function(e){
-    return false;
-});
-	
-	
-$(function() {
-	
-  //initialise form to stop 0 quantity being entered by user
-  
-  $("#quantity_form").validate({
-    //  validation rule
-    rules: {
-      
-      quantity:{
-      	required: true,
-        min: 1
-      },
-     
-     
-    
-    },
-    // error message
-    messages: {
-      quantity: "Must be greater than 0",
-      
-    },
-    errorElement: 'div',
-    errorLabelContainer: '.errortxt',
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
-    submitHandler: function(form) {
-      form.submit();
-    }
-  });
-});	
-
-	});
-
-
-
-
-
